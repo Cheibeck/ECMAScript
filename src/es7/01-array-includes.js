c3d3527eebd09c3d912c04c2af282d8aeb949b08
cheibeck <cabeza_249@hotmail.com>
2023-03-08 09:39:08 -0300

    2.4

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,5 @@
+let numbers =[1, 3, 4, 6, 7, 8, 9]
+console.log(numbers.includes(2))
+
+const list = ['oscar', 'pedro', 'luis', 'tania']
+console.log(list.includes('oscar'))
---
+++ src/es8/00-entries.js
@@ -0,0 +1,5 @@
+const countries = {MX: 'Mexico', CO: 'Colombia', AR: 'Argentina'}
+
+console.log(Object.entries(countries))
+console.log(Object.keys(countries))
+console.log(Object.values(countries))
